9ad36d2a0684b5f78b36fc145ab8bce2ed5cd068
iamprasadraju <iamprasadraju@duck.com>
2026-01-07 12:09:19 +0530

    updated

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ CHAT_ID = getenv("CHAT_ID")
 headers = {
     "User-Agent": (
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "AppleWebKit/537.36 (KHTML, like Gecko)"
         "Chrome/120.0.0.0 Safari/537.36"
     ),
     "Accept-Language": "en-US,en;q=0.9",
